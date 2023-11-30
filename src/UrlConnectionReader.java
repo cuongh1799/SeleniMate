63c3903514851858d80cbbc41784c8fd9bfd45c8
cuongh1799 <cuongghuynhh1009@gmail.com>
2023-12-01 00:04:25 +0700

    it actually worked holy shit

--- src/UrlConnectionReader.java
+++ src/UrlConnectionReader.java
@@ -66,7 +66,7 @@ public class UrlConnectionReader  {
                 }
             }
 
-            Thread.sleep(5000);
+            Thread.sleep(2000);
         }
         catch (InterruptedException e) {
             throw new RuntimeException(e);
